97452e0d6bf5a87fc076b6d6cf5f2d05e500c4cb
Armen <armenhaddade@gmail.com>
2025-07-15 17:08:48 +0200

    rebase with remote

--- server/app.js
+++ server/app.js
@@ -1,20 +1,32 @@
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import auth from './routes/auth.js';
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
 
-const app = express();
+dotenv.config();
+
+var users = require('./routes/users');
+
+var app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 
 app.get('/', (req, res) => {
   res.send('🚀 API is running!');
 });
 
-export default app;
+module.exports = app;
