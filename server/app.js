ed30de50dc1e4fcd522cf1745f73b9254e4de267
Armen <armenhaddade@gmail.com>
2025-07-15 17:08:48 +0200

    Created Mongoose model

--- server/app.js
+++ server/app.js
@@ -2,20 +2,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+
+dotenv.config();
 
 var users = require('./routes/users');
 
 var app = express();
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use('/api/v1/users', users);
 
 app.get('/', (req, res) => {
   res.send('🚀 API is running!');
 });
-
 
 module.exports = app;
