b79cc82e228c3a284dd5d9dfd7b9a3a6c6882f7d
Armen <armenhaddade@gmail.com>
2025-07-15 14:48:28 +0200

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use('/api/v1/users', users);
+
+app.get('/', (req, res) => {
+  res.send('🚀 API is running!');
+});
+
+
+module.exports = app;
