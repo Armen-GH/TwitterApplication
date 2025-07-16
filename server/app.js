6e5e761c3c0190573e01ae4daf8d60e7c6fd7935
Armen <armenhaddade@gmail.com>
2025-07-16 13:11:26 +0200

    minor modifications

--- server/app.js
+++ server/app.js
@@ -1,41 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var dotenv = require('dotenv');
-const { Pool } = require('pg');
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+import auth from './routes/auth.js';
 
 dotenv.config();
 
-const pgPool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
-
-var users = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
+// Connect to PostgreSQL
+const pgPool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
 pgPool.connect()
     .then(() => console.log('PostgreSQL connected'))
-    .catch((err) => console.error('PostgreSQL connection error:', err));
+    .catch(err => console.error('PostgreSQL connection error:', err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/v1/users', users);
+app.use('/api/v1/auth', auth);
 
 app.get('/', (req, res) => {
   res.send('🚀 API is running!');
 });
 
-module.exports = app;
+export default app;
+export { pgPool };
